17eda01544d8285b3b5e4698bffd37afc67a99aa
Rahul Pandita <rpandita1999@outlook.com>
2025-10-24 01:41:08 +0000

    create tests, func to compare user entered password with stored in db pswd

--- internal/auth/hash_check_pswd_test.go
+++ internal/auth/hash_check_pswd_test.go
@@ -19,6 +19,19 @@ func TestHashPassword(t *testing.T) {
 	}
 }
 
+func TestCheckHashPassword(t *testing.T) {
+	pwd := "testpassword456789"
+	hash, err := argon2id.CreateHash(pwd, argon2id.DefaultParams)
+	assertError(t, err, nil)
+
+	match, err := CheckPasswordHash(pwd, hash)
+	assertError(t, err, nil)
+
+	if !match {
+		t.Errorf("match fail: input password %v didn't match hash %v", pwd, hash)
+	}
+}
+
 func assertError(t testing.TB, err_got, err_want error) {
 	t.Helper()
 	if err_got != err_want {
