a1a3f12bb8367869916c027ee17f6a5530d6902c
Rahul Pandita <rpandita1999@outlook.com>
2025-10-24 04:22:04 +0000

    create a function to make and return a jwt

---
+++ internal/auth/make_jwt.go
@@ -0,0 +1,27 @@
+package auth
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	// create a json web token
+	token := jwt.NewWithClaims(
+		jwt.SigningMethodHS256,
+		jwt.RegisteredClaims{
+			Issuer:    "chirpy",
+			IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+			ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(expiresIn)),
+			Subject:   userID.String(),
+		},
+	)
+	// key type for HS256 -> []byte
+	jwt, err := token.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", err
+	}
+	return jwt, nil
+}
